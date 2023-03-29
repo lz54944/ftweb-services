c56ae29e05968e9f0c7447445a62de401c27b719
chenjinhao <13661326043@163.com>
2023-03-29 11:07:55 +0800

    解决时差问题

--- ft-common/ft-common-core/src/main/java/com/hhwy/common/core/web/domain/BaseEntity.java
+++ ft-common/ft-common-core/src/main/java/com/hhwy/common/core/web/domain/BaseEntity.java
@@ -27,7 +27,7 @@ public class BaseEntity implements Serializable {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date createTime;
 
     /**
@@ -38,7 +38,7 @@ public class BaseEntity implements Serializable {
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date updateTime;
 
     /**
